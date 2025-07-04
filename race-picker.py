6f1b6820a7d2aa2942f051f50c2a2bb5f6c98d51
Cody Allen <codyman9202@gmail.com>
2025-07-04 09:50:31 -0400

    added random date

--- race-picker.py
+++ race-picker.py
@@ -20,5 +20,21 @@ while(trackGrade > carGrade):
     selectedTrack = tracklist.iloc[y, 0]
     trackGrade = tracklist.iloc[y, 1]
 
+# picking a random date
+m = random.randrange(1, 12, 1)
+months = ['January', 'February', 'March', 'April', 'May',
+          'June', 'July', 'August', 'September', 'October', 'November', 'December']
+mon = months[m-1]
+
+if (m == 2): maxDate = 28
+elif (m == 9 or m == 4 or m == 6 or m == 11): maxDate =30
+else: maxDate = 31
+
+d = random.randrange(1, maxDate)
+
+date = 'Date: {0} {1}'.format(mon, d)
+
+# print
 print('Car Class:', selectedClass)
 print('Track:', selectedTrack)
+print(date)
